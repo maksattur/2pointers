f2acfa5c2fe77dc110f480e160494f0b69926592
Maksat <turatbekovm@gmail.com>
2023-07-08 22:37:08 +0600

    solutions

---
+++ 26_Remove_Duplicates_from_Sorted_Array/main.go
@@ -0,0 +1,25 @@
+package main
+
+// Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such
+// that each unique element appears only once. The relative order of the elements should be kept the same.
+// Then return the number of unique elements in nums.
+
+// Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+
+// * Change the array nums such that the first k elements of nums contain the unique elements in the order they
+// were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+// * Return k.
+
+// Input: nums = [1,1,2]
+// Output: 2, nums = [1,2,_]
+
+func removeDuplicates(nums []int) int {
+	pointer := 1
+	for i := 1; i < len(nums); i++ {
+		if nums[i] != nums[i-1] {
+			nums[pointer] = nums[i]
+			pointer++
+		}
+	}
+	return pointer
+}
---
+++ 283_Move_Zeroes/main.go
@@ -0,0 +1,22 @@
+package main
+
+// Given an integer array nums, move all 0's to the end of it while maintaining the relative
+// order of the non-zero elements.
+// Note that you must do this in-place without making a copy of the array.
+
+// Input: nums = [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+
+func moveZeroes(nums []int) {
+	pointer := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != 0 {
+			nums[pointer] = nums[i]
+			pointer++
+		}
+	}
+	for pointer < len(nums) {
+		nums[pointer] = 0
+		pointer++
+	}
+}
